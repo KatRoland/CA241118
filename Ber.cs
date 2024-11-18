0bb9af342228164234d238097b60a51918e8d86e
katona.roland <katona.roland@28-DIAK1>
2024-11-18 09:28:57 +0100

    Add project files.

---
+++ Ber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA241118
+{
+    internal class Ber
+    {
+        private string nev { get; set; }
+        private string nem { get; set; }
+        private string reszleg { get; set; }
+        private int belepes { get; set; }
+        private int fizetes { get; set; }
+
+        public Ber(string line)
+        {
+            string[] pieces = line.Split(";");
+            this.nev = pieces[0];
+            this.nem = pieces[1];
+            this.reszleg = pieces[2];
+            this.belepes = Convert.ToInt16(pieces[3]);
+            this.fizetes = Convert.ToInt32(pieces[4]);
+        }
+
+        public override string ToString()
+        {
+            return $"\tNév: {this.getNev()}" +
+                $"\n\tNem: {this.getNem()}" +
+                $"\n\tBelépés: {this.getBelepes()}" +
+                $"\n\tBér: {this.getFizetes().ToString("# ###")} forint";
+        }
+
+        public string getNev()
+        {
+            return this.nev;
+        }
+        public string getNem()
+        {
+            return this.nem;
+        }
+        public string getReszleg()
+        {
+            return this.reszleg;
+        }
+        public int getBelepes()
+        {
+            return this.belepes;
+        }
+
+        public int getFizetes()
+        {
+            return this.fizetes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using CA241118;
+
+List<Ber> berek = [];
+
+FileStream fs = new FileStream("berek2020.txt", FileMode.Open, FileAccess.Read);
+StreamReader sr = new StreamReader(fs, encoding: System.Text.Encoding.UTF8);
+
+sr.ReadLine();
+
+while (!sr.EndOfStream)
+{
+    berek.Add(new Ber(sr.ReadLine()));
+}
+
+sr.Close();
+fs.Close();
+
+Console.Write("3. Feladat: ");
+Console.WriteLine($"Dolgozók száma: {berek.Count()}");
+
+double avgber = berek.Average(e => e.getFizetes())/1000;
+
+Console.Write("4. Feladat: ");
+Console.WriteLine($"Bérek átlaga: {avgber:0.0}");
+
+Console.Write("5. Feladat: ");
+string userinput = "";
+while(userinput == "")
+{
+    Console.Write("Kérem a részleg nevét: ");
+    userinput = Console.ReadLine();
+}
+Console.WriteLine("6. Feladat");
+
+var userreszleg = berek.FindAll(e => e.getReszleg() == userinput);
+if(userreszleg.Count() > 0)
+{
+var fizetes = userreszleg.OrderByDescending(e => e.getFizetes()).ToArray()[0];
+Console.WriteLine(fizetes.ToString());
+
+}
+else
+{
+    Console.WriteLine("A megadott részleg nem letézik");
+}
+
+
+Console.WriteLine("7. Feladat: Statisztika");
+var reszlegek = berek.DistinctBy(e => e.getReszleg()).Select(e => e.getReszleg());
+foreach (var reszleg in reszlegek)
+{
+    int dolgozok = berek.Count(e => e.getReszleg() == reszleg);
+    Console.WriteLine($"\t{reszleg} - {dolgozok} fő");
+}
+
